7cc5e20406bb029e26411655ec33dfc917730ab9
furiousme <shuptaroks@gmail.com>
2024-06-28 06:53:39 +0300

    feat(import-products): create a service and import products file handler

---
+++ ImportService/lib/import_service-stack.ts
@@ -0,0 +1,78 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { CfnOutput, RemovalPolicy } from 'aws-cdk-lib';
+
+import {Bucket, BlockPublicAccess, HttpMethods} from "aws-cdk-lib/aws-s3"
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { join } from 'node:path';
+import { Deployment, LambdaIntegration, LambdaRestApi, RestApi, Stage } from 'aws-cdk-lib/aws-apigateway';
+import { CorsHttpMethod } from 'aws-cdk-lib/aws-apigatewayv2';
+
+
+export class ImportServiceStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+
+    const bucket = new Bucket(this, 'ImportProductsFileBucket', {
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      cors: [{
+        allowedOrigins: ["http://localhost:3000", "https://d131jdsjeiombn.cloudfront.net/"],
+        allowedHeaders: ["*"],
+        maxAge: 3000,
+        allowedMethods: [HttpMethods.PUT, HttpMethods.POST]
+      }]
+    });
+
+
+    const importProductsFile = new NodejsFunction(this, "importProductsFileHandler", {
+      runtime: Runtime.NODEJS_20_X,
+      handler: "handler",
+      entry: join(__dirname + "/handlers/import-products-file/import-products-file.ts"),
+      environment: {
+        UPLOAD_BUCKET_NAME: bucket.bucketName
+      }
+    });
+
+    bucket.grantPut(importProductsFile);
+
+    const restApi = new LambdaRestApi(this, 'ImportServiceRestApi', {
+        handler: importProductsFile,
+        proxy: false,
+      defaultCorsPreflightOptions: {
+        allowMethods: [
+          CorsHttpMethod.GET,
+          CorsHttpMethod.POST,
+          CorsHttpMethod.PUT,
+          CorsHttpMethod.PATCH,
+          CorsHttpMethod.DELETE,
+          CorsHttpMethod.OPTIONS,
+        ],
+        allowHeaders: ['Content-Type', 'X-Amz-Date', 'X-Amz-Security-Token', 'Authorization', 'X-Api-Key', 'X-Requested-With', 'Accept', 'Access-Control-Allow-Methods', 'Access-Control-Allow-Origin', 'Access-Control-Allow-Headers'],
+        allowOrigins: ["*"],
+    }
+  });
+
+  const apiDeployment = new Deployment(this, 'ImportServiceApiDeployment', {api: restApi});
+
+  new Stage(this, 'importServiceDevStage', {deployment: apiDeployment, stageName: "dev"});
+
+  const importResource = restApi.root.addResource('import');
+  const importsIntegration = new LambdaIntegration(importProductsFile)
+
+  importResource.addMethod("GET", importsIntegration,
+     {
+      requestParameters:  {
+        "method.request.querystring.name": true
+      }
+    }
+    );
+
+    new CfnOutput(this, "ImportApiUrl", {
+      value: restApi.url || "",
+    });
+  }
+}
