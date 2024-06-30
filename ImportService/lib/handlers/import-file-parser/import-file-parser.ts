3487fe2dea5cafb5fc56a6b9b6dd25abf11b6628
furiousme <shuptaroks@gmail.com>
2024-06-30 22:13:00 +0300

    test(import-service): add more tests

--- ImportService/lib/handlers/import-file-parser/import-file-parser.ts
+++ ImportService/lib/handlers/import-file-parser/import-file-parser.ts
@@ -12,83 +12,70 @@ export const handler = async (event: FIXME) => {
     console.log("EVENT:", JSON.stringify(event));
 
     const destinationBucket = process.env.DESTINATION_BUCKET_NAME;
+    const processRecords = event.Records.map(async (record: FIXME) => {
+      const bucket = record.s3.bucket.name;
+      const key = record.s3.object.key;
+      const destinationKey = key.replace("uploaded", "parsed");
+
+      const copyParams = {
+        Bucket: destinationBucket,
+        CopySource: `${bucket}/${key}`,
+        Key: destinationKey,
+      };
+      
+      const objectParams = {
+        Bucket: bucket,
+        Key: key,
+      };
+
+      const copyCommand = new CopyObjectCommand(copyParams);
+      const deleteCommand = new DeleteObjectCommand(objectParams);
+      const getCommand = new GetObjectCommand(objectParams);
+      const { Body } = await client.send(getCommand);
+
+      if (!(Body instanceof Readable)) {
+        throw new Error('Body is not a readable stream');
+      }
+      
+      const parsedResult: Product[] = [];
+
+      return new Promise((resolve, reject) => {
+        Body.pipe(csv())
+          .on('data', (data: Product) => parsedResult.push(data))
+          .on('end', async () => {
+            console.log("PARSED DATA:", JSON.stringify(parsedResult));
+
+            try {
+              await client.send(copyCommand);
+              await client.send(deleteCommand);
+
+              console.log("The file was successfully moved");
+            } catch (e) {
+              console.log("Error moving the file:", JSON.stringify(e));
+            }
+
+            resolve(parsedResult);
+          })
+          .on('error', (err: unknown) => reject(err));
+      });
+    });
 
     try {
-        const processRecords = event.Records.map(async (record: FIXME) => {
-          const bucket = record.s3.bucket.name;
-          const key = record.s3.object.key;
-          const destinationKey = key.replace("uploaded", "parsed");
+      await Promise.all(processRecords);
 
-          const copyParams = {
-            Bucket: destinationBucket,
-            CopySource: `${bucket}/${key}`,
-            Key: destinationKey,
-          };
-          
-          const objectParams = {
-            Bucket: bucket,
-            Key: key,
-          };
-
-          const copyCommand = new CopyObjectCommand(copyParams);
-          const deleteCommand = new DeleteObjectCommand(objectParams);
-    
-          const command = new GetObjectCommand(objectParams);
-          const { Body } = await client.send(command);
-    
-          if (!(Body instanceof Readable)) {
-            throw new Error('Body is not a readable stream');
-          }
-          
-          const parsedResult: Product[] = [];
-    
-          return new Promise((resolve, reject) => {
-            Body.pipe(csv())
-              .on('data', (data: Product) => parsedResult.push(data))
-              .on('end', async () => {
-                console.log("PARSED DATA:", JSON.stringify(parsedResult));
-
-                try {
-                  await client.send(copyCommand);
-                  await client.send(deleteCommand);
-
-                  console.log("The file was successfully moved");
-                } catch (e) {
-                  console.log("Error moving the file:", JSON.stringify(e));
-                }
-
-                resolve(parsedResult);
-              })
-              .on('error', (err: unknown) => reject(err));
-          });
-        });
-    
-        await Promise.allSettled(processRecords)
-          .catch(() => {
-            return {
-              statusCode: 500,
-              body: JSON.stringify({
-                success: false,
-                error: { message: "Error processing csv file"}
-              })
-            }
-          })
-
-          return {
-            statusCode: 200,
-            body: JSON.stringify({
-              success: true,
-            }),
-          };
-      } catch (error) {
-        console.error('Error processing S3 events:', error);
-        
-        return {
-          statusCode: 500,
-          body: JSON.stringify({
-            success: false,
-            error: { message: "Internal server error"}
-            })
-        }
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          success: true,
+        }),
+      };
+    } catch (e) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({
+          success: false,
+          error: { message: "Error processing csv file"}
+        })
+      }
     }
 };
