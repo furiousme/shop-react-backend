8713fc7df19cc1dd79bab497384bf143cb7e295e
furiousme <shuptaroks@gmail.com>
2024-07-05 12:28:10 +0300

    feat(product-service): save products from sqs messages to db

--- ProductService/lib/handlers/catalog-batch-process/catalog-batch-process.ts
+++ ProductService/lib/handlers/catalog-batch-process/catalog-batch-process.ts
@@ -1,13 +1,8 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import {SQSBatchItemFailure, SQSEvent} from "aws-lambda";
-import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBDocumentClient, TransactWriteCommand } from "@aws-sdk/lib-dynamodb";
+import { randomUUID } from "node:crypto";
 
-// const defaultHeaders = {
-//     "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,X-Amz-Security-Token,Authorization,X-Api-Key,X-Requested-With,Accept,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Access-Control-Allow-Headers",
-//     "Access-Control-Allow-Origin": "*",
-//     "Access-Control-Allow-Methods": "DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT",
-//     "X-Requested-With": "*"
-// }
 
 const client = new DynamoDBClient();
 const docClient = DynamoDBDocumentClient.from(client);
@@ -17,6 +12,13 @@ export const handler = async (event: SQSEvent) => {
 
     const records = event.Records;
 
+    if (!process.env.PRODUCTS_TABLE_NAME || !process.env.STOCKS_TABLE_NAME) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ success: false, body: "Internal server error"}),
+          };
+    }
+
     if (!records) {
         return {
             statusCode: 400,
@@ -24,25 +26,65 @@ export const handler = async (event: SQSEvent) => {
           };
     }
 
-    const batchItemFailures: SQSBatchItemFailure[] = [];
-    const successJsonObjects = [];
+    try {
+        const batchItemFailures: SQSBatchItemFailure[] = [];
 
-    records.forEach((record )=> {
-        try { 
+        const promises = records.map(async (record) => {
             const jsonObject = JSON.parse(record.body);
             console.log("PARSED OBJECT", jsonObject);
-            successJsonObjects.push(jsonObject);
-        } catch (e) {
-            console.log(e)
 
-            batchItemFailures.push({ itemIdentifier: record.messageId })
-        }
-    });
+            const newProductId = randomUUID();
 
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify({ batchItemFailures }),
-    };
+            const newProduct ={
+                id: newProductId,
+                title: jsonObject.title,
+                description: jsonObject.description,
+                price: jsonObject.price
+            }
+            const newStock = {
+                id: randomUUID(),
+                product_id: newProductId,
+                count: jsonObject.count
+            }
 
-    return response;
+            return docClient.send(new TransactWriteCommand({
+                    TransactItems: [
+                    {
+                        Put: {
+                            TableName: process.env.PRODUCTS_TABLE_NAME,
+                            Item: newProduct,
+                        },
+                    },
+                    {
+                        Put: {
+                            TableName: process.env.STOCKS_TABLE_NAME,
+                            Item: newStock,
+                        },
+                    },
+                    ],
+                }))
+                .then(() => {
+                    console.log("PRODUCT AND STOCK WERE SUCCESSFULLY SAVED");
+                })
+                .catch(e => {
+                    console.log("Error sending items to db", e)
+                    batchItemFailures.push({ itemIdentifier: record.messageId })
+                })
+        })
+
+        await Promise.all(promises);
+
+        console.log("Not handled items:", JSON.stringify(batchItemFailures));
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ batchItemFailures }),
+        };
+    } catch (e) {
+        console.log("ERROR saving items to db", e)
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ success: false, body: "Internal server error"}),
+        };
+    }
 };
