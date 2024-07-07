4da3727c27289e15df0e47c6179bfbd2128cb145
furiousme <shuptaroks@gmail.com>
2024-07-07 17:17:44 +0300

    feat(authorization-service): create authorization service with basic authorizer

--- ImportService/lib/import_service-stack.ts
+++ ImportService/lib/import_service-stack.ts
@@ -7,10 +7,10 @@ import {Bucket, BlockPublicAccess, HttpMethods, EventType} from "aws-cdk-lib/aws
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import { join } from 'node:path';
-import { Deployment, LambdaIntegration, LambdaRestApi, Stage } from 'aws-cdk-lib/aws-apigateway';
+import { AuthorizationType, Deployment, LambdaIntegration, LambdaRestApi, Stage, TokenAuthorizer } from 'aws-cdk-lib/aws-apigateway';
 import { CorsHttpMethod } from 'aws-cdk-lib/aws-apigatewayv2';
 import { LambdaDestination } from 'aws-cdk-lib/aws-s3-notifications';
-import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { Effect, PolicyStatement, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
 import { ConfigProps } from '../../config';
 
 type ImportServiceStackProps = cdk.StackProps & {
@@ -38,7 +38,6 @@ export class ImportServiceStack extends cdk.Stack {
       removalPolicy: RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
     });
-
 
     const importProductsFile = new NodejsFunction(this, "importProductsFileHandler", {
       runtime: Runtime.NODEJS_20_X,
@@ -82,30 +81,50 @@ export class ImportServiceStack extends cdk.Stack {
     const restApi = new LambdaRestApi(this, 'ImportServiceRestApi', {
         handler: importProductsFile,
         proxy: false,
-      defaultCorsPreflightOptions: {
-        allowMethods: [CorsHttpMethod.GET],
-        allowHeaders: ['Content-Type', 'X-Amz-Date', 'X-Amz-Security-Token', 'Authorization', 'X-Api-Key', 'X-Requested-With', 'Accept', 'Access-Control-Allow-Methods', 'Access-Control-Allow-Origin', 'Access-Control-Allow-Headers'],
-        allowOrigins: ["*"],
-    }
-  });
-
-  const apiDeployment = new Deployment(this, 'ImportServiceApiDeployment', {api: restApi});
-
-  new Stage(this, 'importServiceDevStage', {deployment: apiDeployment, stageName: "dev"});
-
-  const importResource = restApi.root.addResource('import');
-  const importsIntegration = new LambdaIntegration(importProductsFile)
-
-  importResource.addMethod("GET", importsIntegration,
-      {
-        requestParameters:  {
-          "method.request.querystring.name": true
-        }
+        defaultCorsPreflightOptions: {
+          allowMethods: [CorsHttpMethod.GET],
+          allowHeaders: ['Content-Type', 'X-Amz-Date', 'X-Amz-Security-Token', 'Authorization', 'X-Api-Key', 'X-Requested-With', 'Accept', 'Access-Control-Allow-Methods', 'Access-Control-Allow-Origin', 'Access-Control-Allow-Headers'],
+          allowOrigins: ["*"],
       }
-  );
+    });
 
-  new CfnOutput(this, "ImportApiUrl", {
-    value: restApi.url || "",
-  });
+    const apiDeployment = new Deployment(this, 'ImportServiceApiDeployment', {api: restApi});
+
+    new Stage(this, 'importServiceDevStage', {deployment: apiDeployment, stageName: "dev"});
+
+    const importsIntegration = new LambdaIntegration(importProductsFile);
+    const basicAuthorizerArn = cdk.Fn.importValue("BasicAuthorizerLambdaArn");
+
+    const authHandler = NodejsFunction.fromFunctionAttributes(this,'BasicAuthorizerLambda', {
+      functionArn: basicAuthorizerArn,
+      sameEnvironment: true
+    })
+
+    const authorizer = new TokenAuthorizer(this, 'BasicTokenAuthorizer', {
+      handler: authHandler,
+      resultsCacheTtl: cdk.Duration.seconds(0),
+    });
+
+    const importResource = restApi.root.addResource('import');
+
+    importResource.addMethod("GET", importsIntegration,
+        {
+          requestParameters:  {
+            "method.request.querystring.name": true
+          },
+          authorizer,
+          authorizationType: AuthorizationType.CUSTOM,
+        }
+    );
+
+    authHandler.addPermission('PermitAPIGatewayInvocation', {
+      principal: new ServicePrincipal('apigateway.amazonaws.com'),
+      sourceArn: restApi.arnForExecuteApi('*')
+    });
+    
+
+    new CfnOutput(this, "ImportApiUrl", {
+      value: restApi.url || "",
+    });
   }
 }
--- config.ts
+++ config.ts
@@ -7,10 +7,12 @@ export type ConfigProps = {
   REGION: string;
   EMAIL_FOR_SNS: string;
   EMAIL_FOR_SNS_SMALL_COUNT: string;
+  FURIOUSME: string;
 };
 
 export const getConfig = (): ConfigProps => ({
   REGION: process.env.REGION || "eu-west-1",
   EMAIL_FOR_SNS: process.env.EMAIL_FOR_SNS || "",
-  EMAIL_FOR_SNS_SMALL_COUNT: process.env.EMAIL_FOR_SNS_SMALL_COUNT || ""
+  EMAIL_FOR_SNS_SMALL_COUNT: process.env.EMAIL_FOR_SNS_SMALL_COUNT || "",
+  FURIOUSME: process.env.FURIOUSME || "NOT PROVIDED",
 });
